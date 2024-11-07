01e026d28985a9c52fb634470d19fa4d8c282421
sabinagrid <sradu@griddynamics.com>
2024-11-07 14:22:21 +0200

    done topic specific tasks

--- 5-0-functional-programming/5-0-1-lambda-functions-map/src/main/java/com/bobocode/fp/Functions.java
+++ 5-0-functional-programming/5-0-1-lambda-functions-map/src/main/java/com/bobocode/fp/Functions.java
@@ -1,12 +1,14 @@
 package com.bobocode.fp;
 
+import static java.lang.Math.abs;
+
 /**
  * An util class that provides a factory method for creating an instance of a {@link FunctionMap} filled with a list
  * of functions.
  * <p>
  * TODO: implement a method and verify it by running {@link FunctionsTest}
  * <p>
- * TODO: if you find this exercise valuable and you want to get more like it, <a href="https://www.patreon.com/bobocode"> 
+ * TODO: if you find this exercise valuable and you want to get more like it, <a href="https://www.patreon.com/bobocode">
  *     please support us on Patreon</a>
  *
  * @author Taras Boychuk
@@ -28,7 +30,11 @@ public class Functions {
     public static FunctionMap<Integer, Integer> intFunctionMap() {
         FunctionMap<Integer, Integer> intFunctionMap = new FunctionMap<>();
 
-        // todo: according to the javadoc add functions using lambda expression
+        intFunctionMap.addFunction("square", n -> n * n);
+        intFunctionMap.addFunction("abs", Math::abs);
+        intFunctionMap.addFunction("increment", n -> n + 1);
+        intFunctionMap.addFunction("decrement", n -> n - 1);
+        intFunctionMap.addFunction("sgn", n -> (n != 0) ? n / abs(n) : 0);
 
         return intFunctionMap;
     }
